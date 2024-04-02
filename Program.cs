5f6408db8ab24c679858ad887606cc8468bb6722
Natasha1324 <165684802+Natasha1324@users.noreply.github.com>
2024-04-01 21:23:46 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_No._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++) 
+            {
+                if (num % 15 == 0) 
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            
+        }
+    }
+}
